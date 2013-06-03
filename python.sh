df26ad90ac71857139183c6b476508b7f412355c
Luis Cabrera Sauco <icorrecam@gmail.com>
2013-06-03 23:35:43 +0100

    Script personalizado (amd77)
    Gestión alternativa para entornos virtuales.
    No tocar O:-)

---
+++ python.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Gestión alternativa de entornos virtuales.
+
+cd $(dirname $0)
+test -e ../virtualenv && VIRTUALENV=../virtualenv
+test -e virtualenv && VIRTUALENV=virtualenv
+export PYTHONPATH=$PYTHONPATH:$(pwd):$(pwd)/apps
+[ "$VIRTUALENV" ] || exec python $*
+source $VIRTUALENV/bin/activate
+exec $VIRTUALENV/bin/python $*
+
