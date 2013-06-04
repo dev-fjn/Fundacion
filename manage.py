b08b2dd646d367a967b5c64ad2cbdcdfa8f42d42
Luis Cabrera Sauco <icorrecam@gmail.com>
2013-06-04 01:03:57 +0100

    Añadidos comentarios

--- manage.py
+++ manage.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''Gestor básico de Django.'''
+
 import os
 import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portal.settings")
+if __name__ == '__main__':
+
+    os.environ.setdefault(
+        'DJANGO_SETTINGS_MODULE',
+        'portal.settings'
+    )
 
     from django.core.management import execute_from_command_line
 
