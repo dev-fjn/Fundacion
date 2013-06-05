8acf89a1780013ffa48b0657911a2b2c467f7401
Luis Cabrera Sauco <icorrecam@gmail.com>
2013-06-05 18:28:09 +0100

    Cambios menores destinados a tocar lo menos posible este fichero.
     * Comentadas las apps admin y admindoc
     * Habilitada la carga de variables en el directorio settings_local.d

--- portal/settings.py
+++ portal/settings.py
@@ -122,9 +122,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    'django.contrib.admin',
+    #'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    'django.contrib.admindocs',
+    #'django.contrib.admindocs',
 )
 
 # A sample logging configuration. The only tangible logging
@@ -164,8 +164,20 @@ import os
 PROJECT_ROOT = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 PROJECT_NAME = 'portal'
 
+# Directorio donde poner recursos comunes
+# a todos los desarrolladores:
 for root, dirs, files in os.walk(os.path.join(PROJECT_ROOT, PROJECT_NAME + '/settings.d')):
     for f in sorted(files):
         full = os.path.join(PROJECT_ROOT, root, f)
         if os.path.isfile(full) and full.endswith(".py"):
             execfile(full)
+
+
+# Directorio donde poner recursos no comunes, es decir, personales
+# de cada uno de los desarrolladores:
+for root, dirs, files in os.walk(os.path.join(PROJECT_ROOT, PROJECT_NAME + '/settings_local.d')):
+    for f in sorted(files):
+        full = os.path.join(PROJECT_ROOT, root, f)
+        if os.path.isfile(full) and full.endswith(".py"):
+            execfile(full)
+
