c510ec3de5555adbb01a52bca3ff01693ef90527
Luis Cabrera sauco <icorrecam@gmail.com>
2013-06-23 12:41:14 +0100

    Comentadas las entradas para el blog. Serán reutilizadas.

--- portal/urls.py
+++ portal/urls.py
@@ -12,7 +12,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
         url(r'^$', 'portada.views.home', name='home'),
-        url(r'^blog/', include('blog.urls')),
+        #url(r'^blog/', include('blog.urls')),
         url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
         url(r'^admin/', include(admin.site.urls)),
         (r'^i18n/', include('django.conf.urls.i18n')),
