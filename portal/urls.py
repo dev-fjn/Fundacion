b7f75f8384a944d0e2c91df97840df066cda61e4
Luis Cabrera Sauco <icorrecam@gmail.com>
2013-06-27 01:12:29 +0100

    Añadido soporte para sitemap.xml

--- portal/urls.py
+++ portal/urls.py
@@ -10,6 +10,18 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from zinnia.sitemaps import TagSitemap
+from zinnia.sitemaps import EntrySitemap
+from zinnia.sitemaps import CategorySitemap
+from zinnia.sitemaps import AuthorSitemap
+
+sitemaps = {
+    'tags': TagSitemap,
+    'blog': EntrySitemap,
+    'authors': AuthorSitemap,
+    'categories': CategorySitemap,
+}
+
 urlpatterns = patterns('',
                        url(r'^$', 'portada.views.home', name='home'),
                        # Blog
@@ -20,6 +32,12 @@ urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
                        (r'^i18n/', include('django.conf.urls.i18n')),
                        )
+
+
+urlpatterns += patterns('django.contrib.sitemaps.views',
+        url(r'^sitemap.xml$', 'index', {'sitemaps': sitemaps}),
+        url(r'^sitemap-(?P<section>.+)\.xml$', 'sitemap', {'sitemaps': sitemaps}),
+        )
 
 #from django.conf import settings
 #if settings.DEBUG and not settings.PRODUCCION:
