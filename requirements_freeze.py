de495559ccc3139b62a4f2578f21658d9615bf51
Luis Cabrera Sauco <icorrecam@gmail.com>
2013-07-13 01:15:19 +0100

    Eliminado django-menu. Añadido, django-menus. Parecidos, pero diferentes.

--- requirements_freeze.py
+++ requirements_freeze.py
@@ -1,54 +1,28 @@
-# Generar requerimientos:
-# ~~~~~~~~~~~~~~~~~~~~~~
-# pip freeze > requirements.txt
-#
-# Instalar requermientos:
-# ~~~~~~~~~~~~~~~~~~~~~~
-# pip install --upgrade -r requirements.txt --use-mirrors
-#
-# Instalar directamente de repositorios:
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# pip install -U -e git://github.com/Fantomas42/django-blog-zinnia.git#egg=django-blog-zinnia
-#
-
-# Fijos:
-# ~~~~~~
+argparse==1.2.1
+beautifulsoup4==4.2.1
+distribute==0.6.34
+Django==1.5.1
+django-debug-toolbar==0.9.4
+django-debug-toolbar-extra==0.0.2
+django-debug-toolbar-user-panel-fc==1.0.1
+django-dtpanel-htmltidy==0.1.1
+django-extensions==1.1.1
+django-menus==1.1.2
+django-modeltranslation==0.6.1
+django-mptt==0.5.5
+django-nose==1.1
+django-tagging==0.3.1
+Fabric==1.6.1
+gunicorn==17.5
+ipython==0.13.2
+nose==1.3.0
+paramiko==1.10.1
 PIL==1.1.7
-pyparsing==1.5.7
-
-# Actualizables:
-# ~~~~~~~~~~~~~~
-argparse>=1.2.1
-BeautifulSoup>=3.2.1
-distribute>=0.6.45
-Django>=1.5.1
-django-blog-zinnia>=0.12.3
-django-debug-toolbar>=0.9.4
-django-debug-toolbar-extra>=0.0.2
-django-debug-toolbar-user-panel-fc>=1.0.1
-django-dtpanel-htmltidy>=0.1.1
-django-extensions>=1.1.1
-django-flatpages-i18n>=0.1.2
-django-menu>=0.1.7
-django-modeltranslation>=0.6.1
-django-mptt>=0.5.5
-django-nose>=1.1
-django-tagging>=0.3.1
-django-xmlrpc>=0.1.5
-docutils>=0.10
-Fabric>=1.6.1
-gunicorn>=0.17.4
-ipython>=0.13.2
-nose>=1.3.0
-paramiko>=1.10.1
-pycrypto>=2.6
-Pygments>=1.6
-python-dateutil>=2.1
-pytidylib>=0.2.1
-pytz>=2013b
-six>=1.3.0
-South>=0.8.1
-vobject>=0.8.1c
-Werkzeug>=0.9.1
-wsgiref>=0.1.2
-
+pycrypto==2.6
+python-dateutil==2.1
+pytidylib==0.2.1
+pytz==2013b
+six==1.3.0
+South==0.8.1
+vobject==0.8.1c
+wsgiref==0.1.2
