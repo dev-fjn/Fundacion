e885287596d9ae5730fed5f7543ab6c0094cd413
Luis Cabrera Sauco <icorrecam@gmail.com>
2013-07-14 16:40:03 +0100

    Añadido modelo para testeo del mptt.

--- portada/models.py
+++ portada/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+# -*- coding: utf-8 -*-
 
-# Create your models here.
+from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+
+class Url(MPTTModel):
+    ''' ... '''
+    name   = models.CharField(max_length=50, unique=True)
+    parent = TreeForeignKey('self', null=True, blank=True, related_name='children')
+    url    = models.CharField(max_length=250, unique=True)
+    lang   = models.CharField(max_length=10)
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
+    def __unicode__(self):
+        return self.name
