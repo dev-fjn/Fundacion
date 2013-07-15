0cd02b6b5d1f054378411e9c70bec84a85554713
Luis Cabrera Sauco <icorrecam@gmail.com>
2013-07-15 13:23:08 +0100

    Añadido soporte para signal: verificar antiguedad de un correo.

--- portada/views.py
+++ portada/views.py
@@ -6,6 +6,8 @@
 
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from datetime import datetime, timedelta    # django_contactme
+from django_contactme import signals        # django_contactme
 
 def home(request):
     '''Ejemplo de vista básica.'''
@@ -17,3 +19,16 @@ def home(request):
 # def plantilla_integrada(request):
 #     html = "<html><head><title></title></head><body><p>Ejemplo de plantilla integrada.</p></body></html>"
 #     return HttpResponse(html)
+
+def check_submit_date_is_within_last_7days(
+    sender,
+    data,
+    request,
+    **kwargs):
+    '''Señal (signal) para ver si es más antiguo que una semana.'''
+    plus7days = timedelta(days=7)
+    if data["submit_date"] + plus7days < datetime.now():
+        return False
+
+signals.confirmation_received.connect(check_submit_date_is_within_last_7days)
+
