113d552244382d7ad52f78b5549dcfb04849001b
Luis Cabrera Sauco <icorrecam@gmail.com>
2013-07-16 10:41:44 +0100

    Archivo de configuración para la gestión de menús.

---
+++ portal/settings.d/apps_django_treemenus.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+'''Django-treemenus
+
+https://pypi.python.org/pypi/django-treemenus/0.8.7
+https://github.com/jphalip/django-treemenus
+'''
+
+INSTALLED_APPS += (
+    'treemenus',
+)
+
+if DEBUG and not PRODUCCION:
+    pass
+else:
+    pass
