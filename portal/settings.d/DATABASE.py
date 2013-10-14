d54f94f3362e26f6446550f86df6c1a91b284e5b
alberto <alberto@machanguillo>
2013-10-14 19:35:22 +0100

    Mas cambios del commit anterior

--- portal/settings.d/DATABASE.py
+++ portal/settings.d/DATABASE.py
@@ -1,10 +1,23 @@
-if DEBUG and not PRODUCCION:
+if DEBUG:
+	# dev y beta
 	DATABASES = {
 		'default': {
 			'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
 			'NAME': PROJECT_ROOT + '/db.sqlite',    # Or path to database file if using sqlite3.
 			'USER': '',                      # Not used with sqlite3.
 			'PASSWORD': '',                  # Not used with sqlite3.
+			'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+			'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+		}
+	}
+else:
+	# produccion
+	DATABASES = {
+		'default': {
+			'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+			'NAME': 'fjn',    # Or path to database file if using sqlite3.
+			'USER': 'fjn',                   # Not used with sqlite3.
+			'PASSWORD': 'fjn',               # Not used with sqlite3.
 			'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
 			'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
 		}
