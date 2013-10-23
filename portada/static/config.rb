0d563d02801178d1987cc4f1f3aed1742d8a01ea
Alberto Morales <amd77@galotecnia.com>
2013-10-23 09:42:51 +0100

    Pasado todo el trabajo de sass de lcabrera

--- portada/static/config.rb
+++ portada/static/config.rb
@@ -1,7 +1,10 @@
 require 'susy'
-#require "rgbapng"
 # Require any additional compass plugins here.
 
+
+# The environment mode. Defaults to :production, can also be :development
+#environment = :development
+environment = :production
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -9,18 +12,22 @@ css_dir = "css"
 sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "js"
-fonts_path = "fonts"
+
+#fonts_dir = "pepe"
+#fonts_path = "" 
+#http_fonts_path = 
+#http_fonts_dir = "pepe"
+
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :expanded
+output_style = (environment == :production) ? :compressed : :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-line_comments = false
-
+line_comments = (environment == :production) ? false : true
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
