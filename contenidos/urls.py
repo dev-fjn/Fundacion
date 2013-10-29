1c90dc50eb56081b9faf248dfc3a2e4386fee338
Alberto Morales <amd77@galotecnia.com>
2013-10-29 19:21:32 +0000

    Enlace a calendario actual

--- contenidos/urls.py
+++ contenidos/urls.py
@@ -5,5 +5,6 @@ from contenidos.views import Calendario, EventoView
 
 urlpatterns = patterns('',
     url(r'calendario/(?P<year>\d+)/(?P<month>\d+)$', Calendario.as_view(), name='calendario'),
+    url(r'calendario/$', Calendario.as_view(), name='calendario'),
     url(r'evento/(?P<pk>\d+)$', EventoView.as_view(), name='evento_view'),
 )
