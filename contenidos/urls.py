36042f31efa5c62b8e5b3aa30f24b451c6a83c22
Fundacion DEV <fjn_dev@fundacionjuannegrin.com>
2013-11-06 18:50:50 +0100

    reindentado

--- contenidos/urls.py
+++ contenidos/urls.py
@@ -6,12 +6,12 @@ from contenidos.models import TIPO
 
 urlpatterns = patterns('',
 	url(r'calendario/(?P<year>\d+)/(?P<month>\d+)/only$', Calendario.as_view(), {'only': True}, name='calendario_only'),
-    url(r'calendario/(?P<year>\d+)/(?P<month>\d+)$', Calendario.as_view(), name='calendario'),
-    url(r'calendario/$', Calendario.as_view(), name='calendario'),
+	url(r'calendario/(?P<year>\d+)/(?P<month>\d+)$', Calendario.as_view(), name='calendario'),
+	url(r'calendario/$', Calendario.as_view(), name='calendario'),
 	url(r'libros/$', Libros.as_view(), name="libros"),
 	url(r'recursos_audiovisuales/$', Documentos.as_view(), {'tipo': TIPO.RECURSOS_AUDIOVISUALES}, name="recursos_audiovisuales"),
 	url(r'presencia_en_prensa/$', Documentos.as_view(), {'tipo': TIPO.PRESENCIA_EN_PRENSA}, name="presencia_en_prensa"),
 	url(r'dossieres_de_prensa/$', Documentos.as_view(), {'tipo': TIPO.DOSSIERES_DE_PRENSA}, name="dossieres_de_prensa"),
-    url(r'evento/(?P<pk>\d+)$', EventoView.as_view(), name='evento_view'),
+	url(r'evento/(?P<pk>\d+)$', EventoView.as_view(), name='evento_view'),
 )
 
