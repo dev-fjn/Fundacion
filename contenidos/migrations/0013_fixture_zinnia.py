439739682471151518272a453a541ef7425040c0
Alberto Morales <amd77@galotecnia.com>
2013-11-07 19:53:43 +0000

    Menu de ultimas actividades externas usando el blog de zinnia

---
+++ contenidos/migrations/0013_fixture_zinnia.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+		from django.core.management import call_command
+		call_command("loaddata", "fixtures/zinnia.json")
+
+    def backwards(self, orm):
+		1/0
+
+    models = {
+        u'contenidos.audioadjunto': {
+            'Meta': {'object_name': 'AudioAdjunto'},
+            'audio': ('filebrowser.fields.FileBrowseField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'documento': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenidos.Documento']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'titulo': ('django.db.models.fields.CharField', [], {'max_length': '250'})
+        },
+        u'contenidos.documento': {
+            'Meta': {'object_name': 'Documento'},
+            'descripcion': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'tipo': ('django.db.models.fields.IntegerField', [], {}),
+            'titulo': ('django.db.models.fields.CharField', [], {'max_length': '250'})
+        },
+        u'contenidos.evento': {
+            'Meta': {'object_name': 'Evento'},
+            'fecha_y_lugar': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'imagen': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'pdf': ('django.db.models.fields.files.FileField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'resumen': ('django.db.models.fields.TextField', [], {}),
+            'titulo': ('django.db.models.fields.CharField', [], {'max_length': '250'})
+        },
+        u'contenidos.fechaevento': {
+            'Meta': {'object_name': 'FechaEvento'},
+            'evento': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenidos.Evento']"}),
+            'fecha': ('django.db.models.fields.DateField', [], {}),
+            'hora_final': ('django.db.models.fields.TimeField', [], {}),
+            'hora_inicio': ('django.db.models.fields.TimeField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        u'contenidos.imagen': {
+            'Meta': {'ordering': "('orden',)", 'object_name': 'Imagen'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'imagen': ('filebrowser.fields.FileBrowseField', [], {'max_length': '200'}),
+            'orden': ('django.db.models.fields.IntegerField', [], {'default': '20'}),
+            'titulo': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'titulo_en': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True', 'blank': 'True'}),
+            'titulo_es': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True', 'blank': 'True'})
+        },
+        u'contenidos.libro': {
+            'Meta': {'object_name': 'Libro'},
+            'autor': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'fecha': ('django.db.models.fields.DateField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'isbn': ('django.db.models.fields.CharField', [], {'max_length': '13'}),
+            'miniatura': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'precio': ('django.db.models.fields.FloatField', [], {}),
+            'resumen': ('django.db.models.fields.TextField', [], {}),
+            'titulo': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'})
+        },
+        u'contenidos.pdfadjunto': {
+            'Meta': {'object_name': 'PdfAdjunto'},
+            'documento': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenidos.Documento']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pdf': ('filebrowser.fields.FileBrowseField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'titulo': ('django.db.models.fields.CharField', [], {'max_length': '250'})
+        },
+        u'contenidos.urladjunto': {
+            'Meta': {'object_name': 'UrlAdjunto'},
+            'documento': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenidos.Documento']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'titulo': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'})
+        },
+        u'contenidos.video': {
+            'Meta': {'object_name': 'Video'},
+            'flv': ('filebrowser.fields.FileBrowseField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'mp4': ('filebrowser.fields.FileBrowseField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'titulo': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'titulo_en': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True', 'blank': 'True'}),
+            'titulo_es': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True', 'blank': 'True'})
+        },
+        u'contenidos.videoadjunto': {
+            'Meta': {'object_name': 'VideoAdjunto'},
+            'documento': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenidos.Documento']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'titulo': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'video': ('filebrowser.fields.FileBrowseField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['contenidos']
+    symmetrical = True
