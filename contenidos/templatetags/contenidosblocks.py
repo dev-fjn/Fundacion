f50af5c4918ccfcbfb40150560230d7c12635c94
Fundacion DEV <fjn_dev@fundacionjuannegrin.com>
2013-11-10 17:25:34 +0000

    Agregar lápices y cruces

--- contenidos/templatetags/contenidosblocks.py
+++ contenidos/templatetags/contenidosblocks.py
@@ -19,34 +19,39 @@ def imagenes():
 def videos():
     return {'MEDIA_URL': settings.MEDIA_URL, "videos": Video.objects.all() }
 
-@register.inclusion_tag('contenidos/_proximos_eventos.html')
-def proximos_eventos(cuenta):
-	cuenta = int(cuenta)
-	start = timezone.now()
-	#end = start+datetime.timedelta(days=60)
-	# buscar eventos en rango
-	qs = FechaEvento.objects.filter(fecha__gte=start) #, fecha__lte=end)
-	return {'object_list': qs[:cuenta]}
+@register.inclusion_tag('contenidos/_proximos_eventos.html', takes_context=True)
+def proximos_eventos(context, cuenta):
+    cuenta = int(cuenta)
+    start = timezone.now()
+    #end = start+datetime.timedelta(days=60)
+    # buscar eventos en rango
+    qs = FechaEvento.objects.filter(fecha__gte=start) #, fecha__lte=end)
+    return {
+            'user': context.get('user'),
+            'object_list': qs[:cuenta], 
+        }
 
-@register.inclusion_tag('contenidos/_calendario_eventos.html')
-def calendario_eventos(meses_atras):
-	start, end = inicio_fin_mes(meses_atras)
-	diccionario = Evento.datos_para_calendario(start, end)
-	semanas = calendario_por_meses(start, end, diccionario)
-	return {
-			'only': True,
-			'hoy': timezone.now().date(), 
-			'start': start, 
-			'semanas': semanas, 
-			'prev': start-datetime.timedelta(days=1), 
-			'next': end+datetime.timedelta(days=1),
-		}
+@register.inclusion_tag('contenidos/_calendario_eventos.html', takes_context=True)
+def calendario_eventos(context, meses_atras):
+    start, end = inicio_fin_mes(meses_atras)
+    diccionario = Evento.datos_para_calendario(start, end)
+    semanas = calendario_por_meses(start, end, diccionario)
+    return {
+            'user': context.get('user'),
+            'only': True,
+            'hoy': timezone.now().date(), 
+            'start': start, 
+            'semanas': semanas, 
+            'prev': start-datetime.timedelta(days=1), 
+            'next': end+datetime.timedelta(days=1),
+        }
 
-@register.inclusion_tag('contenidos/_resumen_de_zinnia.html')
-def resumen_de_zinnia(category_slug):
-	c = Category.objects.get(slug=category_slug)
-	return {
-		'category': c,
-		'object_list': c.entries_published(),
-	}
+@register.inclusion_tag('contenidos/_resumen_de_zinnia.html', takes_context=True)
+def resumen_de_zinnia(context, category_slug):
+    c = Category.objects.get(slug=category_slug)
+    return {
+        'user': context.get('user'),
+        'category': c,
+        'object_list': c.entries_published(),
+    }
 
