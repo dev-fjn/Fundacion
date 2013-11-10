7c857c00e1e462df19a420eed3d2621e66da2446
Fundacion DEV <fjn_dev@fundacionjuannegrin.com>
2013-11-10 11:45:05 +0000

    Retabular y cambios en el modelo libro

---
+++ contenidos/migrations/0020_auto__chg_field_libro_isbn.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Libro.isbn'
+        db.alter_column(u'contenidos_libro', 'isbn', self.gf('django.db.models.fields.CharField')(max_length=17, null=True))
+
+    def backwards(self, orm):
+
+        # Changing field 'Libro.isbn'
+        db.alter_column(u'contenidos_libro', 'isbn', self.gf('django.db.models.fields.CharField')(max_length=13, null=True))
+
+    models = {
+        u'contenidos.audioadjunto': {
+            'Meta': {'object_name': 'AudioAdjunto'},
+            'audio': ('filebrowser.fields.FileBrowseField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'documento': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenidos.Documento']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'titulo': ('django.db.models.fields.CharField', [], {'max_length': '250'})
+        },
+        u'contenidos.documento': {
+            'Meta': {'object_name': 'Documento'},
+            'descripcion': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'tipo': ('django.db.models.fields.IntegerField', [], {}),
+            'titulo': ('django.db.models.fields.CharField', [], {'max_length': '250'})
+        },
+        u'contenidos.evento': {
+            'Meta': {'object_name': 'Evento'},
+            'fecha_y_lugar': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'imagen': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'pdf': ('django.db.models.fields.files.FileField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'resumen': ('django.db.models.fields.TextField', [], {}),
+            'titulo': ('django.db.models.fields.CharField', [], {'max_length': '250'})
+        },
+        u'contenidos.fechaevento': {
+            'Meta': {'object_name': 'FechaEvento'},
+            'evento': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenidos.Evento']"}),
+            'fecha': ('django.db.models.fields.DateField', [], {}),
+            'hora_final': ('django.db.models.fields.TimeField', [], {}),
+            'hora_inicio': ('django.db.models.fields.TimeField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        u'contenidos.imagen': {
+            'Meta': {'ordering': "('orden',)", 'object_name': 'Imagen'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'imagen': ('filebrowser.fields.FileBrowseField', [], {'max_length': '200'}),
+            'orden': ('django.db.models.fields.IntegerField', [], {'default': '20'}),
+            'titulo': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'titulo_en': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True', 'blank': 'True'}),
+            'titulo_es': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True', 'blank': 'True'})
+        },
+        u'contenidos.libro': {
+            'Meta': {'object_name': 'Libro'},
+            'autor': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'editorial': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'fecha': ('django.db.models.fields.DateField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'idioma': ('django.db.models.fields.CharField', [], {'default': "u'Castellano'", 'max_length': '40'}),
+            'isbn': ('django.db.models.fields.CharField', [], {'max_length': '17', 'null': 'True', 'blank': 'True'}),
+            'miniatura': ('filebrowser.fields.FileBrowseField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'pais': ('django.db.models.fields.CharField', [], {'max_length': '40'}),
+            'precio': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'resumen': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'tipo': ('django.db.models.fields.CharField', [], {'default': "u'Libro'", 'max_length': '40'}),
+            'titulo': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'})
+        },
+        u'contenidos.pdfadjunto': {
+            'Meta': {'object_name': 'PdfAdjunto'},
+            'documento': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenidos.Documento']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pdf': ('filebrowser.fields.FileBrowseField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'titulo': ('django.db.models.fields.CharField', [], {'max_length': '250'})
+        },
+        u'contenidos.urladjunto': {
+            'Meta': {'object_name': 'UrlAdjunto'},
+            'documento': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenidos.Documento']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'titulo': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'})
+        },
+        u'contenidos.video': {
+            'Meta': {'object_name': 'Video'},
+            'flv': ('filebrowser.fields.FileBrowseField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'mp4': ('filebrowser.fields.FileBrowseField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'titulo': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'titulo_en': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True', 'blank': 'True'}),
+            'titulo_es': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True', 'blank': 'True'})
+        },
+        u'contenidos.videoadjunto': {
+            'Meta': {'object_name': 'VideoAdjunto'},
+            'documento': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenidos.Documento']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'titulo': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'video': ('filebrowser.fields.FileBrowseField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['contenidos']
--- contenidos/models.py
+++ contenidos/models.py
@@ -11,122 +11,134 @@ from zinnia.models import Category as CategoryZinnia, Entry as EntryZinnia
 
 
 def _siguiente_orden():
-	m = Imagen.objects.aggregate(models.Max("orden"))["orden__max"]
-	return 1 if not m else m+1
+    m = Imagen.objects.aggregate(models.Max("orden"))["orden__max"]
+    return 1 if not m else m+1
 
 class Imagen(models.Model):
-	titulo = models.CharField(max_length=250)
-	orden = models.IntegerField(default=_siguiente_orden)
-	imagen = FileBrowseField("Imagen", max_length=200, directory="imagenes")
+    titulo = models.CharField(max_length=250)
+    orden = models.IntegerField(default=_siguiente_orden)
+    imagen = FileBrowseField("Imagen", max_length=200, directory="imagenes")
 
-	def __unicode__(self):
-		return self.titulo
+    def __unicode__(self):
+        return self.titulo
 
-	class Meta:
-		verbose_name = u'imagen'
-		verbose_name_plural = u'imágenes'
-		ordering = ('orden', )
+    class Meta:
+        verbose_name = u'imagen'
+        verbose_name_plural = u'imágenes'
+        ordering = ('orden', )
 
 class Video(models.Model):
-	titulo = models.CharField(max_length=250)
-	flv = FileBrowseField("FLV", max_length=200, extensions=[".flv",], directory="videos", blank=True, null=True)
-	mp4 = FileBrowseField("MP4", max_length=200, extensions=[".mp4",], directory="videos", blank=True, null=True)
+    titulo = models.CharField(max_length=250)
+    flv = FileBrowseField("FLV", max_length=200, extensions=[".flv",], directory="videos", blank=True, null=True)
+    mp4 = FileBrowseField("MP4", max_length=200, extensions=[".mp4",], directory="videos", blank=True, null=True)
 
-	class Meta:
-		verbose_name = u'vídeo'
-		verbose_name_plural = u'vídeos'
+    class Meta:
+        verbose_name = u'vídeo'
+        verbose_name_plural = u'vídeos'
 
 class Evento(models.Model):
-	titulo = models.CharField(max_length=250)
-	resumen = models.TextField()
-	fecha_y_lugar = models.TextField()
-	imagen = models.ImageField(upload_to="eventos/imagenes", blank=True, null=True)
-	pdf = models.FileField(upload_to="eventos/pdf", blank=True, null=True)
+    titulo = models.CharField(max_length=250)
+    resumen = models.TextField()
+    fecha_y_lugar = models.TextField()
+    imagen = models.ImageField(upload_to="eventos/imagenes", blank=True, null=True)
+    pdf = models.FileField(upload_to="eventos/pdf", blank=True, null=True)
 
-	def __unicode__(self):
-		return u"%s" % (self.titulo, )
-	
-	def fecha_simple(self):
-		return ", ".join([ev.simple() for ev in self.fechaevento_set.all()])
+    def __unicode__(self):
+        return u"%s" % (self.titulo, )
+    
+    def fecha_simple(self):
+        return ", ".join([ev.simple() for ev in self.fechaevento_set.all()])
 
-	@staticmethod
-	def datos_para_calendario(start, end):
-		qs = FechaEvento.objects.filter(fecha__gte=start, fecha__lte=end)
-		# clasificamos los resultados por dias
-		v = {}
-		for fe in qs:
-			dmy = fe.fecha
-			if not dmy in v:
-				v[dmy] = []
-			v[dmy].append(fe.evento)
-		return v
+    @staticmethod
+    def datos_para_calendario(start, end):
+        qs = FechaEvento.objects.filter(fecha__gte=start, fecha__lte=end)
+        # clasificamos los resultados por dias
+        v = {}
+        for fe in qs:
+            dmy = fe.fecha
+            if not dmy in v:
+                v[dmy] = []
+            v[dmy].append(fe.evento)
+        return v
 
 class FechaEvento(models.Model):
-	evento = models.ForeignKey(Evento)
-	fecha = models.DateField()
-	hora_inicio = models.TimeField()
-	hora_final = models.TimeField()
+    evento = models.ForeignKey(Evento)
+    fecha = models.DateField()
+    hora_inicio = models.TimeField()
+    hora_final = models.TimeField()
 
-	def simple(self):
-		return u"%s %s...%s" % (self.fecha, self.hora_inicio, self.hora_final, )
+    def simple(self):
+        return u"%s %s...%s" % (self.fecha, self.hora_inicio, self.hora_final, )
 
-	def __unicode__(self):
-		return u"%s %s-%s (%s)" % (self.fecha, self.hora_inicio, self.hora_final, self.evento)
-
-
-class Libro(models.Model):
-	titulo = models.CharField(max_length=250)
-	autor = models.CharField(max_length=250)
-	isbn = models.CharField(max_length=13)
-	fecha = models.DateField()
-	precio = models.FloatField()
-	miniatura = models.ImageField(upload_to="libros")
-	url = models.URLField(blank=True, null=True)
-	resumen = models.TextField()
-
-	def __unicode__(self):
-		return u"%s (%s)" % (self.titulo, self.autor)
+    def __unicode__(self):
+        return u"%s %s-%s (%s)" % (self.fecha, self.hora_inicio, self.hora_final, self.evento)
 
 class TIPO:
-	RECURSOS_AUDIOVISUALES = 1
-	PRESENCIA_EN_PRENSA = 2
-	DOSSIERES_DE_PRENSA = 3
-	CHOICES = (
-		(RECURSOS_AUDIOVISUALES, 'Recursos Audiovisuales'),
-		(PRESENCIA_EN_PRENSA, 'Presencia en Prensa'),
-		(DOSSIERES_DE_PRENSA, 'Dossieres de Prensa'),
-		)
-	DICT = dict(CHOICES)
-        BASES_HORMIGAS = {
-            RECURSOS_AUDIOVISUALES: 'Fondos y Recursos Documentales',
-            PRESENCIA_EN_PRENSA: 'Fondos y Recursos Documentales',
-            DOSSIERES_DE_PRENSA: 'Sala de Prensa',
-        }
+    PONENCIAS = u'Ponencias'
+    LIBRO = u'Libro'
+    INFORME = u'Informe'
+    BOLETIN = u'Boletín'
+    CATALOGO = u'Catálogo'
+    LIST = [PONENCIAS, LIBRO, INFORME, BOLETIN, CATALOGO]
+    CHOICES = zip(LIST, LIST)
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=250)
+    autor = models.CharField(max_length=250)
+    editorial = models.CharField(max_length=250)
+    tipo = models.CharField(max_length=40, choices=TIPO.CHOICES, default=TIPO.LIBRO)
+    pais = models.CharField(u'país', max_length=40)# , default=u'España')
+    fecha = models.DateField()
+    idioma = models.CharField(max_length=40, default=u'Castellano')
+    precio = models.FloatField(blank=True, null=True)
+    isbn = models.CharField('ISBN', max_length=13+4, blank=True, null=True)
+    miniatura = FileBrowseField("portada", max_length=200, extensions=[".jpg", ".jpeg", ".png", ".gif"], directory="bibliografia", blank=True, null=True)
+    url = models.URLField(blank=True, null=True)
+    resumen = models.TextField(blank=True, null=True)
+
+    def __unicode__(self):
+        return u"%s (%s)" % (self.titulo, self.autor)
+
+class TIPO:
+    RECURSOS_AUDIOVISUALES = 1
+    PRESENCIA_EN_PRENSA = 2
+    DOSSIERES_DE_PRENSA = 3
+    CHOICES = (
+        (RECURSOS_AUDIOVISUALES, 'Recursos Audiovisuales'),
+        (PRESENCIA_EN_PRENSA, 'Presencia en Prensa'),
+        (DOSSIERES_DE_PRENSA, 'Dossieres de Prensa'),
+        )
+    DICT = dict(CHOICES)
+    BASES_HORMIGAS = {
+        RECURSOS_AUDIOVISUALES: 'Fondos y Recursos Documentales',
+        PRESENCIA_EN_PRENSA: 'Fondos y Recursos Documentales',
+        DOSSIERES_DE_PRENSA: 'Sala de Prensa',
+    }
 
 class Documento(models.Model):
-	tipo = models.IntegerField(choices=TIPO.CHOICES)
-	titulo = models.CharField(max_length=250)
-	descripcion = models.TextField()
+    tipo = models.IntegerField(choices=TIPO.CHOICES)
+    titulo = models.CharField(max_length=250)
+    descripcion = models.TextField()
 
-	def adjuntos(self):
-		l = list(self.urladjunto_set.all())
-		l += list(self.pdfadjunto_set.all())
-		l += list(self.videoadjunto_set.all())
-	        l += list(self.audioadjunto_set.all())
-		return sorted(l, key=lambda x: x.titulo)
+    def adjuntos(self):
+        l = list(self.urladjunto_set.all())
+        l += list(self.pdfadjunto_set.all())
+        l += list(self.videoadjunto_set.all())
+        l += list(self.audioadjunto_set.all())
+        return sorted(l, key=lambda x: x.titulo)
 
 class Adjunto(models.Model):
-	documento = models.ForeignKey(Documento)
-	titulo = models.CharField(max_length=250)
+    documento = models.ForeignKey(Documento)
+    titulo = models.CharField(max_length=250)
 
-	def template(self):
-		return "contenidos/_%s.html" % (self.__class__.__name__.lower(), )
+    def template(self):
+        return "contenidos/_%s.html" % (self.__class__.__name__.lower(), )
 
-	def __unicode__(self):
-		return u"%s: %s" % (self.__class__.__name__.upper(), self.titulo)
-	
-	class Meta:
-		abstract = True
+    def __unicode__(self):
+        return u"%s: %s" % (self.__class__.__name__.upper(), self.titulo)
+    
+    class Meta:
+        abstract = True
 
 class UrlAdjunto(Adjunto):
     url = models.URLField()
@@ -159,15 +171,15 @@ class AudioAdjunto(Adjunto):
 @receiver(post_save, sender=Evento)
 def crea_blog_al_guardar_evento(sender, instance, **kwargs):
     if instance:
-		e, created = EntryZinnia.objects.get_or_create(slug="evento-%d" % instance.pk)
-		if created:
-			print "Autocreando entrada", e, e.pk
-			e.title = u"Nuevo evento: %s" % instance.titulo
-			e.content = u"<p>Se va a producir el siguiente evento durante los días %s.</p><p>%s</p>" % (instance.fecha_simple(), instance.resumen)
-			e.excerpt = "Se ha creado un nuevo evento en la agenda"
-			e.categories = CategoryZinnia.objects.filter(slug="actividades-externas")
-			e.sites = Site.objects.all()
-			e.comment_enabled = False
-			e.save()
-		else:
-			print "No sobreescribo entrada existente", e, e.pk
+        e, created = EntryZinnia.objects.get_or_create(slug="evento-%d" % instance.pk)
+        if created:
+            print "Autocreando entrada", e, e.pk
+            e.title = u"Nuevo evento: %s" % instance.titulo
+            e.content = u"<p>Se va a producir el siguiente evento durante los días %s.</p><p>%s</p>" % (instance.fecha_simple(), instance.resumen)
+            e.excerpt = "Se ha creado un nuevo evento en la agenda"
+            e.categories = CategoryZinnia.objects.filter(slug="actividades-externas")
+            e.sites = Site.objects.all()
+            e.comment_enabled = False
+            e.save()
+        else:
+            print "No sobreescribo entrada existente", e, e.pk
