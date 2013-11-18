08a63c69a479fe63c74a83c03df653b69d5f9beb
Fundacion DEV <fjn_dev@fundacionjuannegrin.com>
2013-11-18 17:49:28 +0000

    typo

--- contenidos/migrations/0039_fixtures_categorias.py
+++ contenidos/migrations/0039_fixtures_categorias.py
@@ -8,7 +8,7 @@ class Migration(DataMigration):
 
     def forwards(self, orm):
         from django.core.management import call_command
-        call_command("loaddata", "fixtures/treemenus.json")
+        call_command("loaddata", "fixtures/categorias.json")
 
     def backwards(self, orm):
         "Write your backwards methods here."
