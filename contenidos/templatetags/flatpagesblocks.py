7b6680d71fb0e7149e3066c8cc16c2c91c708557
Fundacion DEV <fjn_dev@fundacionjuannegrin.com>
2014-02-26 14:46:49 +0000

    burbuja para concertar visitas (y a derecha tambien)

---
+++ contenidos/templatetags/flatpagesblocks.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from django import template
+register = template.Library()
+
+from django.conf import settings
+from flatpages_i18n.models import FlatPage_i18n
+
+@register.inclusion_tag('contenidos/_flatblock.html', takes_context=True)
+def flatblock(context, flatpage_url):
+    obj = FlatPage_i18n.objects.get(url=flatpage_url)
+    return {
+        'user': context.get('user'),
+        'object': obj,
+        'flatpage_url': flatpage_url,
+    }
+
