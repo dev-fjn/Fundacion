f2af8e5b4d9ad68ab71f30e5ded55fb0e08bac61
Fundacion DEV <fjn_dev@fundacionjuannegrin.com>
2014-02-26 15:04:11 +0000

    Error cuando abres la / con un browser en ingles

--- contenidos/templatetags/flatpagesblocks.py
+++ contenidos/templatetags/flatpagesblocks.py
@@ -8,7 +8,10 @@ from flatpages_i18n.models import FlatPage_i18n
 
 @register.inclusion_tag('contenidos/_flatblock.html', takes_context=True)
 def flatblock(context, flatpage_url):
-    obj = FlatPage_i18n.objects.get(url=flatpage_url)
+    try:
+        obj = FlatPage_i18n.objects.get(url=flatpage_url)
+    except FlatPage_i18n.DoesNotExist:
+        return {}
     return {
         'user': context.get('user'),
         'object': obj,
